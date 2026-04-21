f2882ab0d50ac6ef63f6555807c74d48f01ce9da
Louis <louis.bagnol@epitech.eu>
2026-04-21 10:17:04 +0200

    feat: implement ShapeFactory for dynamic shape creation using plugins

---
+++ src/factories/ShapeFactory.cpp
@@ -0,0 +1,40 @@
+#include "ShapeFactory.hpp"
+
+PluginLoader ShapeFactory::_pluginLoader;
+std::unordered_map<std::string, void*> ShapeFactory::_createFunctions;
+
+std::shared_ptr<IShape> ShapeFactory::create(const std::string& type, const libconfig::Setting& config, std::shared_ptr<IMaterial> material) {
+    static std::unordered_map<std::string, ShapeCreator> creators = {
+        {"sphere", _createSphere},
+    };
+
+    if (!_ensureLoaded(type)) return nullptr;
+
+    auto it = creators.find(type);
+    if (it != creators.end()) {
+        return it->second(config, material);
+    }
+    return nullptr;
+}
+
+bool ShapeFactory::_ensureLoaded(const std::string& type) {
+    if (_createFunctions.find(type) != _createFunctions.end()) return true;
+
+    std::string pluginPath = "./plugins/shapes/" + type + ".so";
+    if (!_pluginLoader.load(pluginPath)) return false;
+
+    void* createFunc = _pluginLoader.getSymbol(pluginPath, "create");
+    if (!createFunc) return false;
+
+    _createFunctions[type] = createFunc;
+    return true;
+}
+
+std::shared_ptr<IShape> ShapeFactory::_createSphere(const libconfig::Setting& config, std::shared_ptr<IMaterial> material) {
+    double x = config["position"]["x"];
+    double y = config["position"]["y"];
+    double z = config["position"]["z"];
+    double radius = config["radius"];
+    auto createFunc = reinterpret_cast<IShape* (*)(double, double, double, double, std::shared_ptr<IMaterial>*)>(_createFunctions["sphere"]);
+    return std::shared_ptr<IShape>(createFunc(x, y, z, radius, &material));
+}
