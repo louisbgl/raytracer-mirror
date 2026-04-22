fdda222ec23fe3bb882e9e0bbcb6e5e2fc18a596
Lucas Pujol <lucas.pujol@epitech.eu>
2026-04-22 16:36:53 +0200

    feat(material): implement ColoredDiffuse material plugin
    New material that reacts to light color, unlike Lambertian which ignores
    it (flat color per spec). Multiplies surface albedo by incoming light color
    so a white surface under a red light appears red.
    
    Also adds demo scenes: light_colors.txt (4 colored point lights) and
    multi_light.txt (mixed point + directional lights).
    
    Closes #70

---
+++ src/plugins/Materials/ColoredDiffuse.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../Interfaces/IMaterial.hpp"
+
+class ColoredDiffuse : public IMaterial {
+public:
+    ColoredDiffuse(Vec3 albedo);
+
+    Vec3 shade(const HitRecord& record, const Vec3& lightDir, const Vec3& lightColor) const override;
+    bool scatter([[maybe_unused]] const Ray& ray_in, [[maybe_unused]] const HitRecord& record,
+                 [[maybe_unused]] Vec3& attenuation, [[maybe_unused]] Ray& scattered) const override;
+
+private:
+    Vec3 _albedo;
+};
