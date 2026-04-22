8ea3a43fbca0b0ccdcb636fd534cda264d98544d
Louis <louis.bagnol@epitech.eu>
2026-04-22 11:27:34 +0200

    feat: inline solve method in QuadraticSolver class for improved performance

--- src/Math/QuadraticSolver.hpp
+++ src/Math/QuadraticSolver.hpp
@@ -25,7 +25,24 @@ struct QuadraticRoots {
  */
 class QuadraticSolver {
 public:
-    static QuadraticRoots solve(double a, double b, double c);
+    static inline QuadraticRoots solve(double a, double b, double c) {
+        QuadraticRoots roots;
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > EPSILON) {
+            roots.t1 = (-b - std::sqrt(discriminant)) / (2 * a);
+            roots.t2 = (-b + std::sqrt(discriminant)) / (2 * a);
+            roots.count = 2;
+        } else if (std::abs(discriminant) <= EPSILON) {
+            roots.t1 = -b / (2 * a);
+            roots.t2 = roots.t1;
+            roots.count = 1;
+        } else {
+            roots.count = 0;
+        }
+
+        return roots;
+    }
     
 private:
     static constexpr double EPSILON = 1e-8;
