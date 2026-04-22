30c0dd4d7b99245e492b327621f4f8d5f721481f
Louis <louis.bagnol@epitech.eu>
2026-04-22 17:11:59 +0200

    feat: add Torus shape creation to ShapeFactory

--- src/factories/ShapeFactory.hpp
+++ src/factories/ShapeFactory.hpp
@@ -37,7 +37,8 @@ private:
 
     static std::shared_ptr<IShape> _createSphere         (const libconfig::Setting& config, std::shared_ptr<IMaterial> material);
     static std::shared_ptr<IShape> _createCylinder       (const libconfig::Setting& config, std::shared_ptr<IMaterial> material);
-    static std::shared_ptr<IShape> _createRectangle      (const libconfig::Setting& config, std::shared_ptr<IMaterial> material);
     static std::shared_ptr<IShape> _createLimitedCylinder(const libconfig::Setting& config, std::shared_ptr<IMaterial> material);
+    static std::shared_ptr<IShape> _createRectangle      (const libconfig::Setting& config, std::shared_ptr<IMaterial> material);
     static std::shared_ptr<IShape> _createPlane          (const libconfig::Setting& config, std::shared_ptr<IMaterial> material);
+    static std::shared_ptr<IShape> _createTorus          (const libconfig::Setting& config, std::shared_ptr<IMaterial> material);
 };
