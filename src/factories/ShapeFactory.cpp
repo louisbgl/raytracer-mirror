75b4a298bf468ebdc616f48bf7b4c3fe0e218bb1
Gustave Binet <gustave.binet@epitech.eu>
2026-04-22 14:50:58 +0200

    fix(Factory and Cmake): update listing of shapes due to merge conflicts issues

--- src/factories/ShapeFactory.cpp
+++ src/factories/ShapeFactory.cpp
@@ -7,7 +7,7 @@ std::shared_ptr<IShape> ShapeFactory::create(const std::string& type, const libc
     static std::unordered_map<std::string, ShapeCreator> creators = {
         {"sphere", _createSphere},
         {"cylinder", _createCylinder},
-        {"rectangle", _createRectangle}
+        {"rectangle", _createRectangle},
         {"limited_cylinder", _createLimitedCylinder},
         {"plane", _createPlane}
     };
@@ -70,6 +70,8 @@ std::shared_ptr<IShape> ShapeFactory::_createRectangle(const libconfig::Setting&
     double height = config["height"];
     auto createFunc = reinterpret_cast<IShape* (*)(double, double, double, double, double, std::shared_ptr<IMaterial>*)>(_createFunctions["rectangle"]);
     return std::shared_ptr<IShape>(createFunc(x, y, z, width, height, &material));
+}
+
 std::shared_ptr<IShape> ShapeFactory::_createPlane(const libconfig::Setting& config, std::shared_ptr<IMaterial> material) {
     double x = config["position"]["x"];
     double y = config["position"]["y"];
