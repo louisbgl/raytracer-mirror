0a8f58d401f4e8d9081f11dcfa95e25854f6dde9
Lucas Pujol <lucas.pujol@epitech.eu>
2026-04-23 11:54:48 +0200

    feat(core): refactor Core class to integrate logging functionality

--- src/core/Core.hpp
+++ src/core/Core.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "../src/DataTypes/Scene.hpp"
+#include "Image.hpp"
+#include <memory>
 #include <string>
+
+class Logger;
 
 class Core {
 public:
-    Core(std::string inputFile, bool showProgress = false)
-        : _inputFile(std::move(inputFile)), _showProgress(showProgress) {}
-    ~Core() = default;
+    Core(std::string inputFile, bool logging = false);
+    ~Core();
 
     /**
      * @brief Simulates the ray tracing process and renders the scene.
@@ -20,13 +23,18 @@ public:
 
 private:
     std::string _inputFile;
-    bool _showProgress;
+    bool _logging;
+    std::unique_ptr<Logger> _logger;
     Scene _scene;
     double _t_min = 0.001;
     double _t_max = 1000.0;
     Vec3 _baseAmbient = Vec3(25, 25, 38);
     Vec3 _backgroundColor = Vec3(135, 206, 235);
     std::string _outputFile = "output.ppm";
+
+    bool  _loadScene();
+    Image _render();
+    void  _writeOutput(Image& image);
 
     Vec3 trace(const Ray& ray, const Scene& scene, int depth);
 };
