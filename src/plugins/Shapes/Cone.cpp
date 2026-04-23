c45afb07ca6809ea6b21f5ef6355f1731850f6f2
Frederick Jeffcock <frederick.jeffcock@epitech.eu>
2026-04-23 11:26:01 +0200

    refactor(shape): add QuadriticSolver usage

--- src/plugins/Shapes/Cone.cpp
+++ src/plugins/Shapes/Cone.cpp
@@ -1,11 +1,12 @@
 
 #include "./Cone.hpp"
 #include "DataTypes/Vec3.hpp"
-
+#include "../../Math/QuadraticSolver.hpp"
 #include <cmath>
 
 
 
+static constexpr double EPS = 1e-9;
 
 Cone::Cone(Vec3 pos, double radius, std::shared_ptr<IMaterial>material)
 {
@@ -23,41 +24,42 @@ bool Cone::hit(const Ray& ray, double t_min, double t_max, HitRecord& record) co
     double b = 2.0 * (oc.x() * ray.direction().x() + oc.z() * ray.direction().z() - k2 * oc.y() * ray.direction().y());
     double c = (oc.x() * oc.x()) + (oc.z() * oc.z()) - (k2 * oc.y() * oc.y());
 
-    double discriminant = (b * b) - 4 * a * c;
-    if (discriminant < 0) { 
+    if (std::abs(a) < EPS) {
         return false;
     }
 
-    double disc_sqrd = std::sqrt(discriminant);
-    double t = (-b - disc_sqrd) / (2.0 * a);
-    
-    if (t < t_min || t > t_max) {
-        t = (-b + disc_sqrd) / (2.0 * a);
-        if (t < t_min || t > t_max) { 
-            return false;
+    QuadraticRoots roots = QuadraticSolver::solve(a, b, c);
+    if (!roots.hasRoots()) {
+        return false;
+    }
+
+    for (int i = 0; i < roots.count; i++) {
+        double t = roots[i];
+        if (t < t_min || t > t_max) {
+            continue;
         }
+
+        Vec3 hitpoint = ray.at(t);
+        // Gonna assume the "bottom" part of the hourglass for the basic cone
+        if (hitpoint.y() > _position.y() ) {
+            continue;
+        }
+        double rel_x = hitpoint.x() - _position.x();
+        double rel_z = hitpoint.z() - _position.z();
+        double rel_y = hitpoint.y() - _position.y();
+
+    //    double cone_r = std::sqrt(rel_x * rel_x + rel_z * rel_z);
+        Vec3 outward_norm = Vec3(rel_x, -k2 * rel_y, rel_z);
+        outward_norm = normalize(outward_norm);
+
+        record.t = t;
+        record.point = hitpoint;
+        record.material = _material;
+        record.set_face_normal(ray, outward_norm);
+
+        return true;
     }
-
-    Vec3 hitpoint = ray.at(t);
-    
-    // Gonna assume the "bottom" part of the hourglass for the basic cone
-    if (hitpoint.y() > _position.y()) {
-        return false;
-    } 
-
-    double rel_x = hitpoint.x() - _position.x();
-    double rel_z = hitpoint.z() - _position.z();
-    
-    double cone_r = std::sqrt(rel_x * rel_x + rel_z * rel_z);
-    Vec3 outward_norm = Vec3(rel_x, cone_r * _radius, rel_z);
-    outward_norm = normalize(outward_norm);
-
-    record.t = t;
-    record.point = hitpoint;
-    record.material = _material;
-    record.set_face_normal(ray, outward_norm);
-
-    return true;
+    return false;
 }
 
 extern "C" IShape* create(double x, double y, double z, double radius, std::shared_ptr<IMaterial>* material) {
