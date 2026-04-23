bcf0d11583ddd64a142801583f82d848e35c0a9a
Frederick Jeffcock <frederick.jeffcock@epitech.eu>
2026-04-23 11:38:16 +0200

    refactor(shapes): add QuadraticSolver to limitedCone

--- src/plugins/Shapes/LimitedCone.cpp
+++ src/plugins/Shapes/LimitedCone.cpp
@@ -1,6 +1,7 @@
 
 #include "./LimitedCone.hpp"
 #include "DataTypes/Vec3.hpp"
+#include "../../Math/QuadraticSolver.hpp"
 
 #include <cmath>
 #include <iostream>
@@ -23,45 +24,49 @@ bool LimitedCone::hit(const Ray& ray, double t_min, double t_max, HitRecord& rec
     double k_sqrd = k * k;
 
     double a = (ray.direction().x() * ray.direction().x()) + (ray.direction().z() * ray.direction().z()) - (k_sqrd * ray.direction().y() * ray.direction().y());
+    if (std::abs(a) < 1e-9) { 
+        return false;
+    }
+
     double b = 2.0 * (oc.x() * ray.direction().x() + oc.z() * ray.direction().z() - k_sqrd * oc.y() * ray.direction().y());
     double c = (oc.x() * oc.x()) + (oc.z() * oc.z()) - (k_sqrd * oc.y() * oc.y());
 
-    double discriminant = ( b * b) - 4 * a * c;
-    if (discriminant < 0) {
+    QuadraticRoots roots = QuadraticSolver::solve(a, b, c);
+    if (!roots.hasRoots()) {
         return false;
     }
 
-    double disc_sqrd = std::sqrt(discriminant);
-    double t = (-b - disc_sqrd) / (2.0 * a);
-    if (t < t_min || t > t_max) {
-        t = (-b + disc_sqrd) / (2.0 * a);
-        if (t < t_min || t> t_max) {
-            return false;
-        }
-    }
-
-    Vec3 hitpoint = ray.at(t);
     double y_min = _position.y() - _height; 
     double y_max = _position.y();
-    if (hitpoint.y() < y_min || hitpoint.y() > y_max) {
-        return false;
+
+    for (int i = 0; i < roots.count; i++) {
+        double t = roots[i];
+        if (t < t_min || t > t_max) {
+            continue;
+        }
+        Vec3 hitpoint = ray.at(t);
+        if (hitpoint.y() < y_min || hitpoint.y() > y_max) {
+            continue;
+        }
+
+        double rel_x = hitpoint.x() - _position.x();
+        double rel_z = hitpoint.z() - _position.z();
+        double rel_y = hitpoint.y() - _position.y();
+
+        double cone_r = std::sqrt((rel_x * rel_x) + (rel_z * rel_z));
+        double norm_y = (rel_y > 0) ? -cone_r * k : cone_r * k;
+        Vec3 outward_norm = Vec3(rel_x, norm_y, rel_z);
+        outward_norm = normalize(outward_norm);
+
+        record.t = t;
+        record.point = hitpoint;
+        record.material = _material;
+        record.set_face_normal(ray, outward_norm);
+
+        return true;
     }
 
-    double rel_x = hitpoint.x() - _position.x();
-    double rel_z = hitpoint.z() - _position.z();
-    double relative_y = hitpoint.y() - _position.y();
-
-    double cone_r = std::sqrt((rel_x * rel_x) + (rel_z * rel_z));
-    double norm_y = (relative_y > 0) ? -cone_r * k : cone_r * k;
-    Vec3 outward_norm = Vec3(rel_x, norm_y, rel_z);
-    outward_norm = normalize(outward_norm);
-
-    record.t = t;
-    record.point = hitpoint;
-    record.material = _material;
-    record.set_face_normal(ray, outward_norm);
-
-    return true;
+    return false;
 }
 
 extern "C" IShape* create(double x, double y, double z, double radius, double height, std::shared_ptr<IMaterial>* material) {
