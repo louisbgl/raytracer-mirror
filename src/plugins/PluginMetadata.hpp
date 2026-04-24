51c8af49e0df56619487fd650a9ee641ab35221d
Louis <louis.bagnol@epitech.eu>
2026-04-24 14:10:14 +0200

    feat(plugins): add PluginMetadata structure for plugin information

---
+++ src/plugins/PluginMetadata.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+struct PluginMetadata {
+    const char* pluginName;
+    const char* pluralForm;
+    const char* helpText;
+    const char* category; // "shape", "light", "material"
+};
