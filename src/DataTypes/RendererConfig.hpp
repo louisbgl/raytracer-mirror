02950f7cd01ba1fead8ca74339c8a38aa0b757c8
Louis <louis.bagnol@epitech.eu>
2026-04-26 12:12:18 +0200

    feat(config): extend RendererConfig with additional parameters for lighting and background

--- src/DataTypes/RendererConfig.hpp
+++ src/DataTypes/RendererConfig.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
 #include <string>
+#include "Vec3.hpp"
 
 struct RendererConfig {
     bool aaEnabled = false;
     int aaSamples = 1;
     std::string aaMethod = "ssaa";
+
+    double ambientMultiplier = 0.4;
+    double diffuseMultiplier = 0.6;
+
+    Vec3 ambientColor = Vec3(25, 25, 38);
+    Vec3 backgroundColor = Vec3(135, 206, 235);
+    std::string backgroundImage = ""; // If set, this will override backgroundColor
+
+    std::string outputFile = "output.ppm";
 };
