cca4393379ab667bf06e9eb2ada2e708400a3b11
Lucas Pujol <lucas.pujol@epitech.eu>
2026-04-27 18:00:36 +0200

    feat(mesh): update Mesh class to support rotation and translation parameters for OBJ file rendering

--- src/plugins/Shapes/Mesh.cpp
+++ src/plugins/Shapes/Mesh.cpp
@@ -1,33 +1,35 @@
 #include "Mesh.hpp"
 #include "../../parsers/ObjParser.hpp"
 #include "../PluginMetadata.hpp"
+#include <stdexcept>
 
-Mesh::Mesh(const std::string& filename, std::shared_ptr<IMaterial> material) {
+Mesh::Mesh(const std::string& filename, Vec3 rotation, Vec3 translation, std::shared_ptr<IMaterial> material)
+    : AShape(rotation, translation)
+{
     ObjParser parser;
     auto shapes = parser.parse(filename, material);
     if (shapes.empty())
         throw std::runtime_error("Mesh: no triangles loaded from " + filename);
     _bvh = std::make_shared<BVHNode>(shapes, 0, shapes.size());
-    _aabb = _bvh->boundingBox();
 }
 
-bool Mesh::hit(const Ray& ray, double t_min, double t_max, HitRecord& record) const {
-    return _bvh->hit(ray, t_min, t_max, record);
+bool Mesh::hitLocal(const Ray& localRay, HitRecord& record) const {
+    return _bvh->hit(localRay, 0, std::numeric_limits<double>::infinity(), record);
 }
 
-AABB Mesh::boundingBox() const {
-    return _aabb;
+AABB Mesh::computeLocalAABB() const {
+    return _bvh->boundingBox();
 }
 
-extern "C" IShape* create(const char* filename, std::shared_ptr<IMaterial>* material) {
-    return new Mesh(std::string(filename), *material);
+extern "C" IShape* create(const char* filename, double rx, double ry, double rz, double tx, double ty, double tz, std::shared_ptr<IMaterial>* material) {
+    return new Mesh(std::string(filename), Vec3(rx, ry, rz), Vec3(tx, ty, tz), *material);
 }
 
 extern "C" PluginMetadata* metadata() {
     static PluginMetadata metadata = {
         .pluginName = "mesh",
         .pluralForm = "meshes",
-        .helpText = "Mesh (filename, material)",
+        .helpText = "Mesh (filename, rotation, position, material): OBJ mesh with full AShape transform support.",
         .category = "shape"
     };
     return &metadata;
--- src/plugins/Shapes/Mesh.hpp
+++ src/plugins/Shapes/Mesh.hpp
@@ -2,20 +2,19 @@
 
 #include <memory>
 #include <string>
+#include <limits>
 
-#include <Interfaces/IShape.hpp>
-#include <Math/AABB.hpp>
+#include <core/AShape.hpp>
 #include <Math/BVHNode.hpp>
 
-class Mesh : public IShape {
+class Mesh : public AShape {
 public:
-    Mesh(const std::string& filename, std::shared_ptr<IMaterial> material);
+    Mesh(const std::string& filename, Vec3 rotation, Vec3 translation, std::shared_ptr<IMaterial> material);
     ~Mesh() override = default;
 
-    bool hit(const Ray& ray, double t_min, double t_max, HitRecord& record) const override;
-    AABB boundingBox() const override;
+    bool hitLocal(const Ray& localRay, HitRecord& record) const override;
+    AABB computeLocalAABB() const override;
 
 private:
     std::shared_ptr<BVHNode> _bvh;
-    AABB _aabb;
 };
