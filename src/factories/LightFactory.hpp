842f997ca1aa362fa1c00b7253673e25b282bc27
Louis <louis.bagnol@epitech.eu>
2026-05-01 22:37:36 +0200

    feat(light): add support for transformed light creation in LightFactory

--- src/factories/LightFactory.hpp
+++ src/factories/LightFactory.hpp
@@ -2,6 +2,7 @@
 
 #include "../Interfaces/ILight.hpp"
 #include "../core/PluginLoader.hpp"
+#include "../Math/Matrix4x4.hpp"
 #include <libconfig.h++>
 #include <memory>
 #include <string>
@@ -20,9 +21,18 @@ public:
         const libconfig::Setting& config
     );
 
+    static std::shared_ptr<ILight> create(
+        const std::string& type,
+        const libconfig::Setting& config,
+        const Matrix4x4& parentTransform
+    );
+
 private:
     using LightCreator = std::shared_ptr<ILight>(*)(const libconfig::Setting&);
+    using LightCreatorWithTransform = std::shared_ptr<ILight>(*)(const libconfig::Setting&, const Matrix4x4&);
 
     static std::shared_ptr<ILight> _createPointLight(const libconfig::Setting& config);
     static std::shared_ptr<ILight> _createDirectionalLight(const libconfig::Setting& config);
+    static std::shared_ptr<ILight> _createPointLightTransformed(const libconfig::Setting& config, const Matrix4x4& parentTransform);
+    static std::shared_ptr<ILight> _createDirectionalLightTransformed(const libconfig::Setting& config, const Matrix4x4& parentTransform);
 };
