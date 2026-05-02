557029c7f6c68d50077687d38d268808a12fc98b
Louis <louis.bagnol@epitech.eu>
2026-05-02 12:44:30 +0200

    feat(renderer): add tone mapping function using ACES algorithm

--- src/core/RenderSampler.hpp
+++ src/core/RenderSampler.hpp
@@ -10,6 +10,7 @@ public:
 
     static double computeVariance(std::span<const Vec3> colors);
     static Vec3   randomHemisphere(const Vec3& normal);
+    static Vec3   toneMapACES(Vec3 color, double strength = 1.0);
 
 private:
     thread_local static std::mt19937 _gen;
