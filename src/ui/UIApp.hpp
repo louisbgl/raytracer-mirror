1fcc382dde6b9b60a83064acb9fd3b6e48549156
Lucas Pujol <lucas.pujol@epitech.eu>
2026-05-04 10:26:39 +0200

    feat(ui): scaffold UIApp with initial state management and rendering setup

--- src/ui/UIApp.hpp
+++ src/ui/UIApp.hpp
@@ -1,6 +1,20 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <atomic>
+#include <string>
+#include <thread>
+
+#include "PixelBuffer.hpp"
+#include "RenderPanel.hpp"
+#include "SceneBrowser.hpp"
+
+enum class UIState {
+    Browser,
+    Rendering,
+    Done,
+    // FreeRoam
+};
 
 class UIApp {
 public:
@@ -9,7 +23,31 @@ public:
 
     int run();
 
+    static constexpr unsigned int WIN_W = 800;
+    static constexpr unsigned int WIN_H = 600;
+
 private:
+    void handleEvents();
+    void update();
+    void draw();
+
+    void toRendering(const std::string& scenePath);
+    void toBrowser();
+    void toDone();
+
+    void spawnRenderThread();
+    void joinRenderThread();
+
     sf::RenderWindow _window;
-    sf::Event _event;
+    sf::Font         _font;
+    UIState          _state = UIState::Browser;
+
+    SceneBrowser _browser;
+    RenderPanel  _panel;
+
+    std::string       _scenePath;
+    PixelBuffer       _pixelBuffer;
+    std::thread       _renderThread;
+    std::atomic<bool> _cancelFlag{false};
+    std::atomic<bool> _doneFlag{false};
 };
