8ea1691c87aca851f1ea20edc03500cbdd9c673f
Gustave Binet <gustave.binet@epitech.eu>
2026-05-05 14:02:54 +0200

    feat(NormalMapped): implement shade method - decode the pixel + get the pos and build tangent space

--- src/plugins/Materials/NormalMapped.cpp
+++ src/plugins/Materials/NormalMapped.cpp
@@ -23,6 +23,14 @@ Vec3 NormalMapped::shadowTransmittance() const
 Vec3 NormalMapped::shade(const HitRecord& record, const Vec3& lightDir, const Vec3& lightColor,
                          [[maybe_unused]] const Vec3& viewDir) const
 {
+    Vec3 pixel = _normalMap->sample(record.u, record.v);
+    Vec3 tangentNormal = (pixel / 255.0) * 2.0 - Vec3(1.0, 1.0, 1.0);
+
+    Vec3 n = record.normal;
+    Vec3 up = (std::abs(n.y()) < 0.9) ? Vec3(0, 1, 0) : Vec3(1, 0, 0);
+    Vec3 tangent = normalize(up - n * dot(up, n));
+    Vec3 bitangent = cross(n, tangent);
+
     // TODO
     return Vec3(0, 0, 0);
 }
